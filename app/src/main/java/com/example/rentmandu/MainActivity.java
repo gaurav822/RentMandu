bbe9b7a4fccbd9927ce6ef0bc93b88f087eb29ec
Gaurav Dahal <gauravdahal53@gmail.com>
2020-10-12 10:27:12 +0545

    Second Commit

--- app/src/main/java/com/example/rentmandu/MainActivity.java
+++ app/src/main/java/com/example/rentmandu/MainActivity.java
@@ -1,20 +1,27 @@
 package com.example.rentmandu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.annotation.SuppressLint;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
     VideoView videoView;
 
+    CardView cardView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cardView=(CardView) findViewById(R.id.findService);
 
         videoView=(VideoView) findViewById(R.id.video_view);
         String videoPath= "android.resource://" + getPackageName() + "/" + R.raw.demoforrentmandu;
